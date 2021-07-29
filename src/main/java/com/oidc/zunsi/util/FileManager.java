af5f8a8eab4bdb7184f0dc43baf5583a65297acd
bengHak <qkwl4678@naver.com>
2021-07-29 13:53:06 +0900

    feat : 🚧 Implement file manager

---
+++ src/main/java/com/oidc/zunsi/util/FileManager.java
@@ -0,0 +1,23 @@
+package com.oidc.zunsi.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class FileManager {
+    public Optional<File> convertMultipartFileToFile(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
+            try(FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+}
