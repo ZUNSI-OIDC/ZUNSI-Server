344394c0fe2b2b94f921444f3d26613f0a6d58f3
bengHak <qkwl4678@naver.com>
2021-07-29 20:50:26 +0900

    feat : 🚧  review by zunsi api

--- src/main/java/com/oidc/zunsi/controller/v1/ReviewController.java
+++ src/main/java/com/oidc/zunsi/controller/v1/ReviewController.java
@@ -2,10 +2,12 @@ package com.oidc.zunsi.controller.v1;
 
 import com.oidc.zunsi.domain.response.SingleResult;
 import com.oidc.zunsi.domain.user.User;
+import com.oidc.zunsi.domain.zunsi.Zunsi;
 import com.oidc.zunsi.dto.review.ReviewListRowDto;
 import com.oidc.zunsi.service.ResponseService;
 import com.oidc.zunsi.service.ReviewService;
 import com.oidc.zunsi.service.UserService;
+import com.oidc.zunsi.service.ZunsiService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -14,10 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +29,7 @@ import java.util.stream.Collectors;
 public class ReviewController {
 
     private final UserService userService;
+    private final ZunsiService zunsiService;
     private final ReviewService reviewService;
     private final ResponseService responseService;
 
@@ -37,9 +37,19 @@ public class ReviewController {
 
     // review detail Individual Queries
 
-    // review by zunsi
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "jwt 토큰", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "해당 전시 리뷰 리스트")
+    @GetMapping("/zunsi")
+    public ResponseEntity<SingleResult<List<ReviewListRowDto>>> getReviewList(@RequestParam(value = "id") Long zunsiId) {
+        Zunsi zunsi = zunsiService.getZunsiById(zunsiId);
+        List<ReviewListRowDto> dto = reviewService.getReviewByZunsi(zunsi).stream()
+                .map(reviewService::getListRowDto)
+                .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(responseService.getSingleResult(dto));
+    }
 
-    // review by user (jwt token)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "jwt 토큰", required = true, dataType = "String", paramType = "header")
     })
